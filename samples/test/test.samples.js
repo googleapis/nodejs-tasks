901bfe574eccc6bb5256902cb9b189259da3783b
Jonathan Lui <jonathanlui@google.com>
2019-04-04 16:46:51 -0700

    Merge branch master

--- samples/test/test.samples.js
+++ samples/test/test.samples.js
@@ -15,10 +15,8 @@
 'use strict';
 
 const {assert} = require('chai');
-const cp = require('child_process');
+const {execSync} = require('child_process');
 const uuid = require('uuid');
-
-const execSync = (cmd) => cp.execSync(cmd, {encoding: 'utf-8'});
 
 const PROJECT_ID = process.env.GCLOUD_PROJECT;
 const queueName = `gcloud-${uuid.v4().split('-')[0]}`;
